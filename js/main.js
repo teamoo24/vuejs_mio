1fbb19f53be8cc478c26435a98c193f80a63f4f3
Han <601107w@gmail.com>
2019-02-18 12:06:50 +0900

    vuejsの基本追加

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 var app = new Vue({
 	el:'#app',
-	methods: {
-		handleClick: function(event) {
-			alert(event.target)
-		}
+	data: {
+		show: true
 	}
+})
+
+// 基本的なオプションの構成
+var app = new Vue({
+	el:'#app',
+	data:{
+		message:'Vue.js'
+	},
 })
