2d82d030204f934199df8dd667d015af20676372
Han <601107w@gmail.com>
2019-02-20 08:46:33 +0900

    DOMの®使い方

--- js/main.js
+++ js/main.js
@@ -1,33 +1,6 @@
-var app = new Vue({
+new Vue({
 	el:'#app',
-	data: {
-		name: 'キマイラ',
-		list:[
-			{id:1,name:'スライム',hp:100},
-			{id:2,name:'ゴブリン',hp:200},
-			{id:3,name:'オーク',hp:300},
-			{id:4,name:'オーガ',hp:400}
-		]
-	},
-	methods: {
-		//追加ボタンをクリックした時のハンドラ
-		doAdd: function(){
-			//リスト内で一番大きいIDを取得
-			var max = this.list.reduce(function(a,b) {
-				return a > b.id ? a : b.id
-			}, 0)
-			// 新しいモンスターを追加
-			this.list.push({
-				id: max + 1,
-				name: this.name,
-				hp: 500
-			})
-		},
-		doRemove: function(index){
-			this.list.splice(index,1)
-		},
-		attack: function(index){
-			this.list[index].hp -= 10; // hpを減らす
-		}
+	mounted: function(){
+		console.log(this.$el)// -> <div id="app"></div>
 	}
 })
