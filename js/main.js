2a50e0599679c05a165612fb64521226b5ff68ed
Han <601107w@gmail.com>
2019-02-20 13:47:33 +0900

    v-onで出来るイベント集め

--- js/main.js
+++ js/main.js
@@ -3,10 +3,20 @@ var app = new Vue({
 	el:'#app',
 	data:{
 		message : "Hello<strong>Vue.js!</strong>",
+		count:0
 	},
 	methods:{
 		handleClick: function(){
 			alert('クリックしたよ')
+		},
+		handleScroll : function(){
+			alert('スクロールしたよ')
+		},
+		handleWheel : function(){
+			alert('マウスフィールを使ったよ')
+		},
+		view_click : function(){
+			alert(this.count)
 		}
 	}
 })
