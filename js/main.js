e9133e096b3655bcbd2e757eed54dce624087b41
Han <601107w@gmail.com>
2019-02-20 14:26:10 +0900

    修飾子の勉強

--- js/main.js
+++ js/main.js
@@ -2,21 +2,12 @@
 var app = new Vue({
 	el:'#app',
 	data:{
-		message : "Hello<strong>Vue.js!</strong>",
-		count:0
+		message : "Hello Vue.js!"
 	},
 	methods:{
-		handleClick: function(){
-			alert('クリックしたよ')
-		},
-		handleScroll : function(){
-			alert('スクロールしたよ')
-		},
-		handleWheel : function(){
-			alert('マウスフィールを使ったよ')
-		},
-		view_click : function(){
-			alert(this.count)
+		handleInput: function(event) {
+			// 代入前に何か処理を行う
+			this.message = event.target.value
 		}
 	}
 })
