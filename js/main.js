2c313132e462c4dfcddeb05ef530ead6eb1b8e15
Han <601107w@gmail.com>
2019-02-20 14:45:30 +0900

    クリックイベントの勉強

--- js/main.js
+++ js/main.js
@@ -5,9 +5,8 @@ var app = new Vue({
 		message : "Hello Vue.js!"
 	},
 	methods:{
-		handleInput: function(event) {
-			// 代入前に何か処理を行う
-			this.message = event.target.value
+		handler: function(comment) {
+			console.log(comment)
 		}
 	}
 })
